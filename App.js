8180fafcc5143389ab6d1cb434709d14ba7d4a63
G00377211@atu.ie <G00377211@atu.ie@ENG209>
2023-03-30 18:06:10 +0100

    Starting Project for Mobile App

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Button, Text, View } from 'react-native'
+import {useState} from 'react'
 
-export default function App() {
+const Stack = createNativeStackNavigator()
+
+export default App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen
+          name="Fetch"
+          component={FetchScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Go to Fetch Screen"
+      onPress={() => navigation.navigate('Fetch')
+      }
+    />
+  )
+}
+
+const FetchScreen = ({ navigation }) => {
+  const [text, setText] = useState('. . . waiting for fetch API')
+
+  const callAPI = async () => {
+    try {
+      const res = await fetch(
+        `https://a04a-193-1-57-1.eu.ngrok.io`,
+        {
+          method: 'GET',
+          headers: {
+            "ngrok-skip-browser-warning": "69420"
+          },
+        }
+      );
+      const data = await res.json();
+      console.log(data);
+      setText(JSON.stringify(data))
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+  <View>
+    <Text>{text}</Text>
+    <Button
+      title="Go Fetch Some Quotes" onPress={async () => callAPI()}
+    />
+  </View>
+  )
+};
